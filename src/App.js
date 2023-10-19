8fc5705a938c6d4c67cfc6377fc967580f7bf93e
EnguerranSGG <enguerran.smagghe@hotmail.fr>
2023-10-19 22:56:52 +0200

    Create router and had style to nav bar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,43 @@ import './App.css';
 import { isEmpty } from './components/utils';
 import Product from './components/products';
 import NewProduct from './components/newProduct';
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Header />,
+    errorElement: <h1>Page not found</h1>,
+    children: [
+      {
+        path: 'Tous',
+        element: <h1>Tous les articles</h1>
+      },
+      {
+        path: 'Hommes',
+        element: <h1>Hommes</h1>
+      },
+      {
+        path: 'Femmes',
+        element: <h1>Femmes</h1>
+      },
+      {
+        path: 'Ados',
+        element: <h1>Ados</h1>
+      },
+      {
+        path: 'Bebes',
+        element: <h1>Bébés</h1>
+      }
+    ]
+  }
+]);
 function App() {
   const products = useSelector(state => state.productsReducer);
 
   return (
     <>
-      <Header />
+      <RouterProvider router={router} />
       <NewProduct />
       <main>
         <div className='products'>
@@ -31,6 +61,14 @@ function Header() {
       <div className='h2'>
         <h2> Inventory Management</h2>
       </div>
+      <nav>
+        <NavLink to={'/'}>Tous les articles</NavLink>
+        <NavLink to={'/Hommes'}>Hommes</NavLink>
+        <NavLink to={'/Femmes'}>Femmes</NavLink>
+        <NavLink to={'/Ados'}>Ados</NavLink>
+        <NavLink to={'/Bebes'}>Bébés</NavLink>
+      </nav>
+      <div><Outlet /></div>
     </header>
   );
 }
